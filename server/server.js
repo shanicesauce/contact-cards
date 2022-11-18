52912bf15ec25f0c0951a66bc7bf04c8684e08fd
shanicesauce <shanicesauceda@gmail.com>
2022-11-18 00:06:52 -0600

    added webpacks. serves up site

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('../client/'))
+app.use(express.static('../client/dist/'))
 app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
